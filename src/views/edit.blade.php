a7fafb907d09ae43e16a2646fa361f3843c07595
Francesco Passanante <francescopassanante@gmail.com>
2024-03-03 16:47:11 +0100

    fix: comment line to hide note-insert summernote

--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -2,69 +2,72 @@
 @section('title', trans('crud.edit', ['item' => $page->title]))
 @section('title_header', trans('crud.edit', ['item' => $page->title]))
 @section('buttons')
-<a href="{{url('admin/pages')}}" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="{{trans('generals.back')}}"><i class="fas fa-chevron-left"></i></a>
+    <a href="{{ url('admin/pages') }}" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom"
+        title="{{ trans('generals.back') }}"><i class="fas fa-chevron-left"></i></a>
 @endsection
 @section('content')
-<div class="container">
-    @if ($errors->any())
-    @include('admin.partials.errors', ['errors' => $errors])
-    @endif
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <form action="{{route('pages.update', $page->id)}}" method="POST" enctype="multipart/form-data">
-                        @method('PATCH')
-                        @csrf
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <strong>{{trans('tables.title')}}</strong>
-                                    <input type="text" name="title" class="form-control" value="{{ $page->title }}">
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <strong>{{trans('forms.description')}}</strong>
-                                    <textarea name="description" class="form-control">{{ $page->description }}</textarea>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <strong>{{trans('forms.content')}}</strong>
-                                    <textarea name="content" class="form-control summernote">{{ $page->content }}</textarea>
-                                </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <div class="icheck-primary d-inline">
-                                        <input type="checkbox" id="is_published" name="is_published" {{ $page->is_published ? 'checked' : ''}}>
-                                        <label for="is_published">{{trans('forms.publish')}}</label>
+    <div class="container">
+        @if ($errors->any())
+            @include('admin.partials.errors', ['errors' => $errors])
+        @endif
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ route('pages.update', $page->id) }}" method="POST" enctype="multipart/form-data">
+                            @method('PATCH')
+                            @csrf
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>{{ trans('tables.title') }}</strong>
+                                        <input type="text" name="title" class="form-control"
+                                            value="{{ $page->title }}">
                                     </div>
                                 </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>{{ trans('forms.description') }}</strong>
+                                        <textarea name="description" class="form-control">{{ $page->description }}</textarea>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>{{ trans('forms.content') }}</strong>
+                                        <textarea name="content" class="form-control summernote">{{ $page->content }}</textarea>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <div class="icheck-primary d-inline">
+                                            <input type="checkbox" id="is_published" name="is_published"
+                                                {{ $page->is_published ? 'checked' : '' }}>
+                                            <label for="is_published">{{ trans('forms.publish') }}</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary">{{ trans('generals.save') }}</button>
+                                </div>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                                <button type="submit" class="btn btn-primary">{{trans('generals.save')}}</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-<script>
-    $(document).ready(function(){
-        $('.summernote').summernote({
-            callbacks: {
-                onImageUpload: function(files) {
-                    sendFile(files[0], $(this));
+    <script>
+        $(document).ready(function() {
+            $('.summernote').summernote({
+                callbacks: {
+                    onImageUpload: function(files) {
+                        sendFile(files[0], $(this));
+                    }
                 }
-            }
+            });
+            // $('.note-btn-group.btn-group.note-insert').hide()
         });
-        $('.note-btn-group.btn-group.note-insert').hide()
-    });
-</script>
+    </script>
 @endsection
